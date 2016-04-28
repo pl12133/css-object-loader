42d2d656fe7348b45efd2b414b6f588ed14afec8
pl12133 <krirken@gmail.com>
2016-04-28 16:00:49 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+function camelCase(word) {
+  return word.split('').reduce((memo, current, index, arr) => {
+    if (current !== '-') {
+      var letter = (arr[index-1] === '-') ? current.toUpperCase() : current;
+      memo += letter;
+    }
+    return memo;
+  }, '');
+}
+module.exports = function(source) {
+  console.log('Loaded something', source);
+  var parsed = source
+    // Split the style block into lines
+    .split('\n')
+    // Remove lines that don't include rules
+    .filter(line => line.includes(';'))
+    // Join rules to string to perform regular expression
+    .join(' ')
+    // Trim
+    .trim()
+    // Replace all repeating whitespace with one whitespace character
+    .replace(/\s\+/g, ' ')
+    // Rules are delimited by semicolon. Split string into rules
+    .split(';')
+    // Reduce rules into an object with camel cased keys
+    .reduce((memo, current) => {
+      var ruleTuple = current.split(':')
+      var key = camelCase(ruleTuple[0]).trim();
+      var value = ("" + ruleTuple[1]).trim();
+      if (key) {
+        memo[key] = value;
+      }
+      return memo;
+    }, {});
+  console.log('Got Styles:', source);
+  console.log('Parsed to:', parsed);
+  return 'module.exports = ' + JSON.stringify(parsed) + ';';
+};
